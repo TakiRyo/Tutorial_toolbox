5dc2bceacc6da7953bac05adee067e678a18e0f7
Ryosei Takiguchi <takiryo00614@gmail.com>
2026-04-20 19:17:48 +0900

    feat: add date to make

--- tools/config.py
+++ tools/config.py
@@ -1,4 +1,4 @@
 TOOLS = [
-    {"name": "おみくじ・占い", "module": "tool_a"},
-    {"name": "画像背景除去・フィルター", "module": "tool_b"},
+    {"name": "おみくじ・占い", "module": "tool_omikuji"},
+    {"name": "画像背景除去・フィルター", "module": "tool_remove_background"},
 ]
